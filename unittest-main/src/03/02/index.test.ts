85ea2edbaa824a9f7e0ea143f5107a7b5eea4636
chameleon05119 <chameleon05119@gmail.com>
2024-02-11 15:41:53 +0900

    減算の関数とテストの追加

--- unittest-main/src/03/02/index.test.ts
+++ unittest-main/src/03/02/index.test.ts
@@ -1,10 +1,20 @@
-import { add } from "./";
+import { add, sub } from "./";
 
-describe("add", () => {
-  test("1 + 1 = 2", () => {
-    expect(add(1, 1)).toBe(2);
+describe("四則演算", () => {
+  describe("add", () => {
+    test("1 + 1 = 2", () => {
+      expect(add(1, 1)).toBe(2);
+    });
+    test("1 + 2 = 3", () => {
+      expect(add(1, 2)).toBe(3);
+    });
   });
-  test("1 + 2 = 3", () => {
-    expect(add(1, 2)).toBe(3);
+  describe("sub", () => {
+    test("3 - 2 = 1", () => {
+      expect(sub(3, 2)).toBe(1);
+    });
+    test("1 - 2 = -1", () => {
+      expect(sub(1, 2)).toBe(-1);
+    });
   });
 });
--- unittest-main/src/03/02/index.ts
+++ unittest-main/src/03/02/index.ts
@@ -2,4 +2,9 @@ export function add(a: number, b: number) {
   return a + b;
 }
 
+export function sub(a: number, b: number) {
+  return a - b;
+}
+
 add(1, 2);
+sub(3, 2);
