8c41f546ae2ad6eebf975ef47102d79408678d9a
chameleon05119 <chameleon05119@gmail.com>
2024-02-17 10:48:16 +0900

    外部APIを実行するファイルの追加

---
+++ unittest-main/src/04/03/fetchers.ts
@@ -0,0 +1,20 @@
+export type Profile = {
+  id: string;
+  name?: string;
+  age: number;
+  email: string;
+};
+
+export function getMyProfile(): Promise<Profile> {
+  return fetch("https://myapi.testing.com/my/profile").then(async (res) => {
+    const data = await res.json();
+    if (!res.ok) {
+      throw data;
+    }
+    return data;
+  });
+}
+
+export async function getGreet(): Promise<string> {
+  return "Hello, anonymous user!";
+}
