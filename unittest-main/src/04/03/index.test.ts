40457957dd548dce811e514b7a365889eda0a1a3
chameleon05119 <chameleon05119@gmail.com>
2024-02-17 10:50:16 +0900

    プロフィール取得のAPIでnameが返ってこなかった場合は定形文を返すテストの追加

---
+++ unittest-main/src/04/03/index.test.ts
@@ -0,0 +1,24 @@
+import * as Fetchers from "./fetchers";
+import { getGreet } from "./fetchers";
+
+jest.mock("./fetchers", () => {
+  return {
+    ...jest.requireActual("./fetchers"),
+    getGreet: jest.fn().mockResolvedValue("Hello, anonymous user!"),
+  };
+});
+jest.spyOn(Fetchers, "getMyProfile").mockResolvedValueOnce({
+  id: "xxxxxxxx-123456",
+  age: 20,
+  email: "taroyamada@myapi.testing.com",
+});
+
+test("データ取得成功時：ユーザー名がない場合は定型文を返す。", async () => {
+  // getMyProfileが成功した時の値を再現
+  jest.spyOn(Fetchers, "getMyProfile").mockResolvedValueOnce({
+    id: "xxxxxxxx-123456",
+    age: 20,
+    email: "taroyamada@myapi.testing.com",
+  });
+  await expect(getGreet()).resolves.toBe("Hello, anonymous user!");
+});
