8fc2961a30a51ee9fa9e0f5be07fd11759ba5b15
chameleon05119 <chameleon05119@gmail.com>
2024-02-23 10:35:42 +0900

    記事投稿一覧を取得するAPIの実装

---
+++ unittest-main/src/04/04/fetchers.ts
@@ -0,0 +1,25 @@
+export type Article = {
+  id: string;
+  createdAt: string;
+  tags: string[];
+  title: string;
+  body: string;
+};
+
+export type Articles = {
+  articles: Article[];
+};
+
+const host = (path: string) => `https://myapi.testing.com${path}`;
+
+async function handleResponse(res: Response) {
+  const data = await res.json();
+  if (!res.ok) {
+    throw data;
+  }
+  return data;
+}
+
+export function getMyArticles(): Promise<Articles> {
+  return fetch(host("/my/articles")).then(handleResponse);
+}
