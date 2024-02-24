11b17079f3fb9fa0d84e7d0f1b37b84edab5b6c1
chameleon05119 <chameleon05119@gmail.com>
2024-02-25 08:32:44 +0900

    propsのnameがDOM要素に含まれるか確認

--- unittest-main/src/05/03/Form.test.tsx
+++ unittest-main/src/05/03/Form.test.tsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Form } from "./Form";
 
 test("名前の表示", () => {
   render(<Form name="taro" />);
+  // レンダリングされた結果に、"taro"という文字列が含まれたDOM要素が含まれるか確認
+  expect(screen.getByText("taro")).toBeInTheDocument();
 });
