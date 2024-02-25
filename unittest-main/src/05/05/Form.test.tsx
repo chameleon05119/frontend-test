4bc5142f15cdfd608d636af5aecf299d5bd1951f
chameleon05119 <chameleon05119@gmail.com>
2024-02-25 10:11:36 +0900

    「利用規約の同意」ボタン押下時のテストを追加

--- unittest-main/src/05/05/Form.test.tsx
+++ unittest-main/src/05/05/Form.test.tsx
@@ -1,7 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Form } from "./Form";
+
+const user = userEvent.setup();
 
 test("初期状態では「サインアップ」ボタンが非活性", () => {
   render(<Form />);
   expect(screen.getByRole("button", { name: "サインアップ" })).toBeDisabled();
+});
+
+test("「利用規約の同意」チェックボックスを押下すると「サインアップ」ボタンが活性状態になる", async () => {
+  render(<Form />);
+  await user.click(screen.getByRole("checkbox"));
+  expect(screen.getByRole("button", { name: "サインアップ" })).toBeEnabled();
 });
