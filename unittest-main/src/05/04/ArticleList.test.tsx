a9edff39626ed59a7083220f850a10daa2ed7e7b
chameleon05119 <chameleon05119@gmail.com>
2024-02-25 09:10:18 +0900

    itemsが空の場合のテストを追加

--- unittest-main/src/05/04/ArticleList.test.tsx
+++ unittest-main/src/05/04/ArticleList.test.tsx
@@ -9,3 +9,13 @@ test("itemsの数だけ一覧表示される", () => {
   // withinでテスト対象のulを絞り込む
   expect(within(list).getAllByRole("listitem")).toHaveLength(3);
 });
+
+test("itemsが空の場合、「投稿記事がありません」が表示", () => {
+  render(<ArticleList items={[]} />);
+  // getByRoleでは指定した要素が見つからない場合テストがエラー終了するため、
+  // queryByRoleを使用する
+  const list = screen.queryByRole("list");
+  expect(list).not.toBeInTheDocument();
+  expect(list).toBeNull();
+  expect(screen.getByText("投稿記事がありません。")).toBeInTheDocument();
+});
