0003af9383326ce9437d1c2d558c05df0fa8423e
chameleon05119 <chameleon05119@gmail.com>
2024-02-25 09:34:38 +0900

    fieldsetのアクセシブルネームがlegend要素によって決まることを確認するテストの追加

---
+++ unittest-main/src/05/05/Agreement.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import { Agreement } from "./Agreement";
+
+test("fieldsetのアクセシブルネームは、legendを引用している", () => {
+  render(<Agreement />);
+  // <fieldset>要素のロールは"group"
+  // <legend>に指定された文字列が<fieldset>のアクセシブルネームになる
+  expect(
+    screen.getByRole("group", { name: "利用規約の同意" })
+  ).toBeInTheDocument();
+});
